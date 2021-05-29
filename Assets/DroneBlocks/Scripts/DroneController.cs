1c4a78440ea5db777029f5a19d62c1c3f625c3d2
Dennis Baldwin <dennisbaldwin@gmail.com>
2021-05-29 17:40:09 -0500

    Adding pitch roll yaw with Lerp

--- Assets/DroneBlocks/Scripts/DroneController.cs
+++ Assets/DroneBlocks/Scripts/DroneController.cs
@@ -14,10 +14,14 @@ namespace DroneBlocks
         [SerializeField] private float minMaxPitch = 30f;
         [SerializeField] private float minMaxRoll = 30f;
         [SerializeField] private float yawPower = 4f;
+        [SerializeField] private float lerpSpeed = 2f;
 
         private DroneInputs inputs;
-
         private List<IEngine> engines = new List<IEngine>();
+
+        private float finalPitch;
+        private float finalRoll;
+        private float finalYaw;
         #endregion
 
         #region Main Methods
@@ -48,7 +52,30 @@ namespace DroneBlocks
 
         private void HandleControls()
         {
+            // Input is between -1 to 1 to we multiply
+            float pitch = inputs.Cyclic.y * minMaxPitch;
+            float roll = -inputs.Cyclic.x * minMaxRoll;
+            float yaw = inputs.Pedals * yawPower;
 
+            finalPitch = Mathf.Lerp(finalPitch, pitch, Time.deltaTime * lerpSpeed);
+            finalRoll = Mathf.Lerp(finalRoll, roll, Time.deltaTime * lerpSpeed);
+            finalYaw = Mathf.Lerp(finalYaw, yaw, Time.deltaTime * lerpSpeed);
+
+            // https://docs.unity3d.com/ScriptReference/Quaternion.Euler.html
+            // public static Quaternion Euler(float x, float y, float z);
+            // Pitch rotates around the X axis
+            // Yaw rotates around the Y axis
+            // Roll rotates around the Z axis
+            Quaternion rot = Quaternion.Euler(finalPitch, finalYaw, finalRoll);
+
+            // public void AddTorque(float x, float y, float z, ForceMode mode = ForceMode.Force);
+            // public void MoveRotation(Quaternion rot);
+
+            rb.MoveRotation(rot);
+
+            // This will be more realistic but harder to control since there will be no limit
+            // TODO: work with this later
+            //rb.AddTorque(pitch, yaw, roll, ForceMode.Force);
         }
 
 
